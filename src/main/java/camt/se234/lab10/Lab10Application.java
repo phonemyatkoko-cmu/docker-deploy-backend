2850ddcb7f32e253f923a5996760f0292ffbe737
Phone <hponemyut@gmail.com>
2025-02-28 09:06:13 +0700

    uncommented

--- src/main/java/camt/se234/lab10/Lab10Application.java
+++ src/main/java/camt/se234/lab10/Lab10Application.java
@@ -2,11 +2,9 @@ package camt.se234.lab10;
 
 @SpringBootApplication
 public class Lab10Application {
-//some comments
 
     public static void main(String[] args) {
         SpringApplication.run(Lab10Application.class, args);
     }
 
-//added a comment in app
 }
